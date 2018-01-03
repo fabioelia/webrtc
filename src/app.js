462c78f0e56275e0b82d0bd0622ed29c575904c9
Fabio Elia <fabio@f1v.co>
2018-01-03 11:59:59 -0500

    stream suppport

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ var _ = require('lodash');
 
 var url = new URL(location.href);
 var params = url.searchParams;
-
-
 var options = {};
 var isTeacher = params.get('teacher');
+
 if (isTeacher) {
   var c = document.getElementById("canvas");
   var ctx = c.getContext("2d");
@@ -15,17 +14,35 @@ if (isTeacher) {
   ctx.arc(100, 75, 50, 0, 2 * Math.PI);
   ctx.stroke();
   options.initiator = true;
-  options.stream = c.captureStream();
+  options.stream = c.captureStream(0);
+  var i = 10;
+  setInterval(function(){
+    ctx.arc(100 + i, 75 + i, 50 + i, 0, 2 * Math.PI);
+    ctx.stroke();
+    i += 4;
+  }, 200);
 }
 
 var hub = signalhub('teacher-example', ['https://signalhub-jccqtwhdwc.now.sh'])
 var sw = swarm(hub, options);
-hub.broadcast('teacher-example', {hello: 'world'})
 
 sw.on('peer', function (peer, id) {
-  options;
   console.log('connected to a new peer:', id);
   console.log('total peers:', sw.peers.length);
+  peer.on('data', function (data) {
+    // got a data channel message
+    console.log('got a message from peer: ' + data)
+  });
+  peer.on('stream', function (stream) {
+    // got remote video stream, now let's show it in a video tag
+    var video = document.querySelector('video')
+    video.srcObject = stream; //window.URL.createObjectURL(stream)
+    video.onloadedmetadata = function(e) {
+      // test;
+      console.log("TEST");
+    }
+    video.play();
+  })
 })
 
 sw.on('disconnect', function (peer, id) {
@@ -33,4 +50,10 @@ sw.on('disconnect', function (peer, id) {
   console.log('total peers:', sw.peers.length);
 })
 
+function message() {
+  _.each(sw.peers, function(peer) {
+    peer.send('xyz');
+  });
+}
+window.message = message;
 window.sw = sw;
